82afa4bc6864bceea3f69e457e1480ccaacbd778
Tim Middleton <tim@vaults.ca>
2013-04-17 14:53:33 -0400

    add template processors, misc

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for dinner project.
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -165,3 +166,6 @@ LOGGING = {
         },
     }
 }
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + ( 'django.core.context_processors.request',)
+
